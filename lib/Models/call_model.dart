94dfaa0186d68ffa4bd2ec4937c111b6bac62743
Juan Manuel <jvillavicenciot26@outlook.com.pe>
2023-02-11 00:38:26 -0500

    Cambios en llamadas

---
+++ lib/Models/call_model.dart
@@ -0,0 +1,13 @@
+class CallModel {
+  String name;
+  String date;
+  String condition;
+  String type;
+
+  CallModel({
+    required this.name,
+    required this.date,
+    required this.condition,
+    required this.type,
+  });
+}
